a7278f3d5ed2f21eccd4df6b5188d0636aca4dba
Void-CA <castilloari282@gmail.com>
2025-09-30 13:29:33 -0600

    feature(core): linkedList and DoublyLinkedList implemented on core

--- src/bindings/mod.rs
+++ src/bindings/mod.rs
@@ -2,10 +2,11 @@ use pyo3::prelude::*;
 use pyo3::types::PyModule;
 
 pub mod boolean_algebra;
+pub mod data_structures;
 pub mod matrixes;
 
 pub fn register_modules(parent: &Bound<'_, PyModule>) -> PyResult<()> {
     boolean_algebra::register(parent)?;
-
+    data_structures::register(parent)?;
     Ok(())
 }
--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,6 +1,8 @@
 // src/core/mod.rs
 pub mod boolean_algebra;
+pub mod data_structures;
 pub mod matrixes;
 
 // Re-export para fácil acceso
 pub use boolean_algebra::{BooleanExpr};
+
